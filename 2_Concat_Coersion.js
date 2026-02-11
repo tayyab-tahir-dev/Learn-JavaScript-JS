bc3f70c82a91ff306ba92f51e90c4ba753f3073c
tayyab <tt0370260@gmail.com>
2026-02-11 12:17:43 +0500

    Add New Lines Of Code

--- 2_Concat_Coersion.js
+++ 2_Concat_Coersion.js
@@ -1,16 +1,16 @@
 // CONCATENATION IN JAVASCRIPT..........
 
 // In JavaScript we use the + operator for arithmetic addition, but we also use for string conncatination.
-
+// do ya do se zyada strings ko aapas mein jorna aksar + operator ki madad se.
 
 // const str = "Hello " + "World";
 // console.log(str);
 
 
 
-// const sum = "Tayyab" -5;
-// console.log(sum);
-
+// TYPE COERCION IN JAVASCRIPT..............
+// jab JavaScript khud-ba-khud (automatically) ek data type ko doosre data type mein convert kar deti hai, taake operation perform ho sake.
+// JS bolti hai: “chalo type change kar dete hain, kaam ho jaye”
 
 
 // const sum = "5"  -5;
@@ -18,5 +18,21 @@
 
 
 
-// const sum = "5"  +10;
+// const sum = "5"  + 10;
 // console.log(sum);
+
+
+// SOME TRICKY QUESTIONS......
+
+
+// console.log(10 + "20");
+// console.log(9 - "5");
+// console.log("Java" + "Script");
+// console.log(" " + " ");
+// console.log(" " + 0);
+// console.log("Tayyab" - "Tahir");
+// console.log(true + true);
+// console.log(true + false);
+// console.log(false + true);
+// console.log(false - true);
+
