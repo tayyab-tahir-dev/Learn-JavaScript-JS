8287d609c07023e2640b26ff440ed0801fdb560f
tayyab <tt0370260@gmail.com>
2026-02-16 11:59:39 +0500

    Example Of Functions

---
+++ 5_Functions.js
@@ -0,0 +1,20 @@
+// A Function is a block of code that performs a specific task. It can be reused multiple times in a program, which helps to avoid code duplication and makes the code more organized and easier to maintain.
+// A Fuction Is Used To Make Our Code Reusable.
+
+
+// SYNTAX...................
+
+// function functionName(parameters) {
+//     // code to be executed
+// }
+
+// EXAMPLE..................
+
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(5, 5));
+console.log(sum(10, 15));
+console.log(sum(10, 50));
+
+
