1f2afdd4ed85e8cb1d0af6cbbe1c285f9926340b
tayyab <tt0370260@gmail.com>
2026-02-19 10:35:12 +0500

    Learn About Var, let And Const

--- es2015.js
+++ es2015.js
@@ -1,2 +1,50 @@
 // LET AND CONST --- ECMASCRIPT 2015
 
+// LET KEYWORD: 
+// THE LET KEYWORD IS USED TO DECLARE VARIALE WITH BLOCK SCOPE. VARIABLE DECLARED WITH LET ARE MUTABLE, MEANING THEIR VALUE CAN BE REASSIGNED.
+
+
+
+// CONST KEYWORD:
+// THE CONST KEYWORD IS USED TO DECLARE VARIABLES WITH BLOCK SCOPE, BUT ONCE A VALUE IS ASSIGNED TO A CONST VARIABLE, IT CANNOT BBE REASSIGNED.
+// CONST VARIBLES ARE IMMUTABLE.
+
+// SOME EXAMPLES...........
+
+// EX # 1 -- BY VAR KEYWORD......
+
+
+// if (true) {
+//     var myName = "Tayyab";
+//     console.log(myName);
+    
+// }
+
+// myName = "Tahir";
+// console.log(myName);
+
+
+
+
+
+// if (true) {
+//     let myName = "Tayyab";
+//     console.log(myName);
+    
+// }
+
+// myName = "Tahir";
+// console.log(myName);
+
+
+// EX # 3 -- BY CONST KEYWORD......
+
+
+// if (true) {
+//     const myName = "Tayyab";
+//     console.log(myName);
+    
+// }
+
+// myName = "Tahir";
+// console.log(myName);
