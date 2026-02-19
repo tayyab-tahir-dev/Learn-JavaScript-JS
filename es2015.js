8ceabb693f2a41e1b3847b58f0bbd7bd049cdc42
tayyab <tt0370260@gmail.com>
2026-02-19 10:56:32 +0500

    Learn Template Strings And String Interpolation

--- es2015.js
+++ es2015.js
@@ -10,7 +10,6 @@
 // CONST VARIBLES ARE IMMUTABLE.
 
 // SOME EXAMPLES...........
-
 // EX # 1 -- BY VAR KEYWORD......
 
 
@@ -19,7 +18,6 @@
 //     console.log(myName);
     
 // }
-
 // myName = "Tahir";
 // console.log(myName);
 
@@ -32,7 +30,6 @@
 //     console.log(myName);
     
 // }
-
 // myName = "Tahir";
 // console.log(myName);
 
@@ -45,6 +42,29 @@
 //     console.log(myName);
     
 // }
-
 // myName = "Tahir";
 // console.log(myName);
+
+
+
+// TEMPLATE STRINGS -- ECMASCRIPT 2015...........
+// IN ECMASCRIPT 6 (ES6), TEMPLATE STRINGS, ALSO KNOWN AS TEMPLATE LITERALS, PROVIDES A CONVENIENT AND FLEXIBLE EAY TO CREATE A STINGS IN JAVASCRIPT. TEMPLATE STRINGS ARE ENCLOSED IN BACKTICK (``) RATHER THEN SINGLE OR DOUBLE QUOTES.
+
+// EXAMPLE......
+
+// let firstName = "Tayyab";
+// let lastName = "Tahir";
+
+// let fullName = `${firstName} ${lastName}`
+// console.log(fullName);
+
+
+
+// STRING INTERPOLATION: TEMPLATES STINGS SUPPORT STRING INTERPOLAR, ALLOWING YOU TO EMBED EXPRESSION DIRECTLY WITHIN THE STRING, INTERPOLATED EXPRESSIONS ARE ENCLOSED IN ${}
+
+// EXAMPLE.................
+
+// let age = 18;
+
+// let message = `I am ${age} Years Old`
+// console.log(message);
